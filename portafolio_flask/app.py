a65fa4ac816880e0d1279496febde4e3b69e352e
CamiloRojas9005 <102090627+CamiloRojas9005@users.noreply.github.com>
2024-10-14 19:24:51 -0500

    up

--- portafolio_flask/app.py
+++ portafolio_flask/app.py
@@ -10,19 +10,19 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     # return 'este es mi portafoloio Camilo rojas'
-    return render_template('index.html')
+    return render_template('index.html', active_page='home')
 
 @app.route('/about')
 def about():
-    return 'about' 
+    return render_template('about.html', active_page='about')
 
 @app.route('/projects')
 def projects():
-    return 'aqui van los proyectos que hago '
+    return render_template('projects.html', active_page='projects')
 
 @app.route('/contact')
 def contact():
-    return 'Contacto'
+    return render_template('contact.html', active_page='contact')
 
 if __name__ == '__main__':
     app.run(debug=True)
